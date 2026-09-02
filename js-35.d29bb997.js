783bc8d9d32d1f62baa9f9342630d82f71b48ac8
pavlo20031 <lesivpavlo999@gmail.com>
2026-09-02 22:54:24 +0300

    Updates

--- js-35.d29bb997.js
+++ js-35.d29bb997.js
@@ -766,12 +766,15 @@ const makeTransaction = (transaction)=>{
         const delay = randomIntegerFromInterval(200, 500);
         setTimeout(()=>{
             const canProcess = Math.random() > 0.3;
-            if (canProcess) onSuccess(transaction.id, delay);
+            if (canProcess) onSuccess({
+                id: transaction.id,
+                time: delay
+            });
             else onError(transaction.id);
         }, delay);
     });
 };
-const logSuccess = (id, time)=>{
+const logSuccess = ({ id, time })=>{
     console.log(`Transaction ${id} processed in ${time}ms`);
 };
 const logError = (id)=>{
